7435123083937c2604951cc23f81135b4dfdb674
Jakub Toroszewski <s31835@pjwstk.edu.pl>
2025-06-13 15:29:43 +0200

    aa

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using ExampleTest2.Data;
-//using ExampleTest2.Services;
+using ExampleTest2.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatabaseContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-//builder.Services.AddScoped<IDbService, DbService>();
+builder.Services.AddScoped<IDbService, DbService>();
 
 var app = builder.Build();
 
@@ -26,29 +26,3 @@ app.MapControllers();
 
 app.Run();
 
-/*
- * cmd -> sqllocaldb info (MSSQLLOCALBD powinno być) lub i
---------nie ma---------
-sqllocaldb create MSSQLLocalDB |
-sqllocaldb start MSSQLLocalDB
-sqlcmd -S "(localdb)\MSSQLLocalDB" | uruchom klienta
-CREATE DATABASE apbd;
-GO
-sqlcmd -S '(localdb)\MSSQLLocalDB' -i create.sql
-EXIT
-
-SELECT name FROM sys.databases; /sprawdzenie jakie mamy bazy
-GO
-------------------------------------------------------
-Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=apbd;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False
-
-
-dotnet add package Microsoft.EntityFrameworkCore
-dotnet add package Microsoft.EntityFrameworkCore.SqlServer
-dotnet add package Microsoft.EntityFrameworkCore.Tools
-
-dotnet tool install --global dotnet-ef
-
-dotnet ef migrations add InitialCreate
-dotnet ef database update
- */
